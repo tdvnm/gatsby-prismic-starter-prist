402ff52c90ffb7b6b804846e0e73667af2db00b2
shubhro <shubhrogupta117@gmail.com>
2019-11-05 13:45:59 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
         resolve: 'gatsby-source-prismic-graphql',
         options: {
-            repositoryName: 'prist', // (REQUIRED, replace with your own)
+            repositoryName: 'prist', shubhro
             linkResolver: () => post => `/${post.uid}`,
         }
     },
